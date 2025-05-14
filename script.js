86470ea3ee70d5d880aa1e89fd49a6add617e66c
AlimMak <alimmak04@gmail.com>
2025-05-13 23:00:38 -0500

    linked the style sheet cleaned up the javascript a little bit still needs more work

--- script.js
+++ script.js
@@ -3,19 +3,30 @@ submitbutton.addEventListener("click", addExpense);
 clearbutton = document.getElementById("clearButton");
 clearbutton.addEventListener("click", clearList)
 
-
+let totalArray = [];
 
 
 
 function addExpense(){
+    // access the list and create the element that we will be appending
     ExpenseList = document.getElementById("displayExpenses");
-    singleExpense = document.createElement("div");
-    singleExpense.textContent = document.getElementById("dollaramt").value;
-    singleDescription = document.createElement("div");
-    singleDescription.textContent = document.getElementById("expenseDesc").value;
-    ExpenseList.appendChild(singleExpense);
-    ExpenseList.appendChild(singleDescription);
-    console.log("added expense")
+    singleExpense = document.createElement("div")
+    
+    // create element to store dollar amount and append 
+    ExpenseCost = document.createElement("span");
+    ExpenseCost.textContent = document.getElementById("dollaramt").value;
+    singleExpense.appendChild(ExpenseCost);
+
+    // create element to store the expense description and append 
+    ExpenseDescription = document.createElement("div");
+    ExpenseDescription.textContent = document.getElementById("expenseDesc").value;
+    singleExpense.appendChild(ExpenseDescription);
+    
+    ExpenseList
+
+    totalArray.push(document.getElementById("dollaramt").value);
+    console.log(totalArray);
+    console.log("added expense");
 }
 
 
