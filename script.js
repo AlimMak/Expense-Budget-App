cb0b6d85b303246f261f9421df67078fe8881e3a
AlimMak <alimmak04@gmail.com>
2025-05-14 02:57:58 -0500

    added most of the features need to work on the dropdown menu to distinguish expense and income

--- script.js
+++ script.js
@@ -4,29 +4,45 @@ clearbutton = document.getElementById("clearButton");
 clearbutton.addEventListener("click", clearList)
 
 let totalArray = [];
-
-
+/*
+totalAmount = document.getElementById("expenseTotal");
+totalToAdd = document.createElement("h2");
+totalToAdd.textContent = calculateTotal();
+totalAmount.appendChild(totalToAdd);
+*/
+calculateTotal();
 
 function addExpense(){
     // access the list and create the element that we will be appending
     ExpenseList = document.getElementById("displayExpenses");
     singleExpense = document.createElement("div")
+    singleExpense.className = "singleExp"
     
     // create element to store dollar amount and append 
-    ExpenseCost = document.createElement("span");
+    ExpenseCost = document.createElement("h3");
     ExpenseCost.textContent = document.getElementById("dollaramt").value;
+    ExpenseCost.className = "expenseAmount"
     singleExpense.appendChild(ExpenseCost);
 
     // create element to store the expense description and append 
-    ExpenseDescription = document.createElement("div");
+    ExpenseDescription = document.createElement("h3");
     ExpenseDescription.textContent = document.getElementById("expenseDesc").value;
     singleExpense.appendChild(ExpenseDescription);
+
+    removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.className = "remove-button";
+    removeButton.addEventListener("click", removeExpense);
+    singleExpense.appendChild(removeButton);
+
     
-    ExpenseList
+    ExpenseList.appendChild(singleExpense)
 
     totalArray.push(document.getElementById("dollaramt").value);
     console.log(totalArray);
     console.log("added expense");
+    calculateTotal();
+
 }
 
 
@@ -36,4 +52,36 @@ function clearList(){
     listTitle = document.createElement("h1");
     listTitle.textContent = "Expense List:"
     ExpenseList.appendChild(listTitle);
+    calculateTotal();
+}
+
+function removeExpense(){
+    const expenseToRemove = this.parentNode;
+    const amountToRemove = expenseToRemove.querySelector("h3").textContent;
+
+
+    //totalArray = totalArray.filter(item => item !== amountToRemove);
+    firstIndex = totalArray.indexOf(amountToRemove);
+    totalArray.splice(firstIndex, 1);
+    
+
+    expenseToRemove.remove();
+    console.log("removed expense");
+    console.log(totalArray)
+    calculateTotal();
+}
+
+function calculateTotal(){
+    totalAmount = document.getElementById("expenseTotal");
+    totalAmount.innerHTML = "";
+    totalToAdd = document.createElement("h2");
+    totalDifference = 0; 
+    for(let i = 0; i < totalArray.length; i++){
+        currInt = parseFloat(totalArray[i]);
+        totalDifference += currInt;
+        console.log(typeof totalArray[i]);
+        console.log(totalDifference);
+    }
+    totalToAdd.textContent = totalDifference;
+    totalAmount.appendChild(totalToAdd);
 }
